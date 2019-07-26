e2201195180b5d896b0eb71bd19560322022e3a1
mohanadElmaghrby1 <mohanad2021996@gmail.com>
2019-07-26 16:58:23 +0200

    add initial data

--- src/main/java/mohannad/springframework/controllers/IndexController.java
+++ src/main/java/mohannad/springframework/controllers/IndexController.java
@@ -1,13 +1,32 @@
 package mohannad.springframework.controllers;
 
+import mohannad.springframework.model.Category;
+import mohannad.springframework.model.UnitOfMeasure;
+import mohannad.springframework.repositories.CategoryRepository;
+import mohannad.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"" , "/" ,"/index","/index.html"})
     public String getIndexPage(){
+        Optional<Category> categoryOptional =categoryRepository.findByDescription("Italian");
+        Optional<UnitOfMeasure> unomOptional =unitOfMeasureRepository.findByDescription("Pinch");
+        System.out.println("cat italian id : "+categoryOptional.get().getId());
+        System.out.println("unom Pinch id : "+ unomOptional.get().getId());
+
         return "index";
     }
 }
